9a491fc753bf98f03570e01a34cfb6420f802294
hasparus <hasparus@gmail.com>
2019-05-29 23:55:34 +0200

    🎉 Upload presentation

---
+++ theme.js
@@ -0,0 +1,14 @@
+export const theme = {
+  font: "monospace",
+  li: {
+    fontSize: "1.5rem",
+    margin: "1rem 0",
+  },
+  blockquote: {
+    fontStyle: "italic",
+    backgroundColor: "#f8f8f8",
+    padding: "1rem",
+    border: "1px solid black",
+    boxShadow: "0.5rem 0.9rem 0 0.2rem",
+  },
+};
