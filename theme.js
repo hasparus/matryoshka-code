b44e9dd630e0f11589046c43308e34c63554f075
hasparus <hasparus@gmail.com>
2020-04-28 19:47:33 +0200

    Add Simpsons fragment

--- theme.js
+++ theme.js
@@ -1,14 +1,27 @@
 export const theme = {
-  font: "monospace",
-  li: {
-    fontSize: "1.5rem",
-    margin: "1rem 0",
+  fonts: {
+    body: "monospace",
   },
-  blockquote: {
-    fontStyle: "italic",
-    backgroundColor: "#f8f8f8",
-    padding: "1rem",
-    border: "1px solid black",
-    boxShadow: "0.5rem 0.9rem 0 0.2rem",
+  colors: {
+    text: "black",
+    background: "white",
+    primary: "blue",
+  },
+  styles: {
+    li: {
+      fontSize: "1.5rem",
+      margin: "1rem 0",
+    },
+    blockquote: {
+      fontStyle: "italic",
+      backgroundColor: "#f8f8f8",
+      padding: "1rem",
+      border: "1px solid black",
+      boxShadow: "0.5rem 0.9rem 0 0.2rem",
+    },
+    img: {
+      width: "unset",
+      height: "unset",
+    },
   },
 };
